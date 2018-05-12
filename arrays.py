a2facebb1ac9aaf73156ed71c08e384ac1bd94a1
nasty11pig <50954250@qq.com>
2018-05-12 19:50:23 +0800

    512

---
+++ arrays.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 12 19:39:04 2018
+
+@author: zjy
+"""
+
+class Arrays(object):
+    
+    def __init__(self, capacity, fillValue = None):
+        self._items = list()
+        for count in range(capacity):
+            self._items.append(fillValue)
+            
+    
+    def __iter__(self):
+        return iter(self._items)
+    
+    def __getitem__(self, index):
+        return self._items[index]
+    
+    def __str__(self):
+        return str(self._items)
+    
+    def __setitem__(self, index, newitem):
+        self._items[index] = newitem
--- node.py
+++ node.py
@@ -26,3 +26,23 @@ for count in range(1, 6):
     head = Node(count, head)       
 
 
+class TwoWayNode(Node):
+    
+    def __init__(self, data, previous = None, next = None):
+        Node.__init__(self, data, next)
+        self.previous = previous
+        
+
+head = TwoWayNode(1)
+tail = head
+
+for data in range(2,6):
+    tail.next = TwoWayNode(data,tail)
+    tail = tail.next
+    
+
+probe = tail
+while probe != None:
+    print(probe.data)
+    probe = probe.previous
+    
